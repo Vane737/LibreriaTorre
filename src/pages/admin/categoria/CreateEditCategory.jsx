a22795a3ee91f1e40eeb23f598fc86fe6da44653
dianaM <diana.montano@assuresoftbolivia.com>
2023-07-17 23:07:27 -0400

    Add CreateEdit category

---
+++ src/pages/admin/categoria/CreateEditCategory.jsx
@@ -0,0 +1,77 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+import api from "../../../API/axios";
+import { useForm } from "react-hook-form";
+
+export default function CreateEditRol() {
+  let { id } = useParams();
+  const navigate = useNavigate();
+
+  //form
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
+  
+  //handlers
+  const handleProviderSubmit = (data) => {
+    if(id){
+      //editar
+      api.put(`/usuario/categorias/${id}`, data)
+      .then((res) => {
+        console.log(res);
+        navigate("/admin/categorias");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }else{
+      //crear
+      api.post("/usuario/categorias", data).
+      then((res) => {
+        console.log(res);
+        navigate("/admin/categorias");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+  //render
+  return (
+    <div className="container mx-auto w-3/6">
+      <h1 className="text-2xl font-bold text-center mb-4 w-full pt-5">
+        {id ? "EDITAR CATEGORIA" : "REGISTRAR CATEGORIA"}
+      </h1>
+      <form onSubmit={handleSubmit(handleProviderSubmit)}>
+        <div className="mb-4">
+          <label htmlFor="nombre" className="block mb-2 w-full">
+            Nombre
+          </label>
+          <input
+            type="text"
+            {...register("nombre")}
+            className="border border-gray-300 px-3 py-2 w-full rounded-md"
+          />
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="bg-custom-green rounded-md p-2 block w-full mb-4"
+          >
+            {id ? "Editar" : "Registrar"}
+          </button>
+
+          <Link
+            type="button" 
+            className="bg-custom-red rounded-md text-center p-2 block w-full"
+            to="/admin/categorias"
+          >
+            Cancelar
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
